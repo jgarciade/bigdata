a5527f8812a0eb0a8632f03e5d066294e93141aa
Jose Garcia <>
2019-06-18 04:23:58 -0500

    adding data base creation script

---
+++ mongo/database_creation.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+
+mongoimport --db stackoverflow --collection survey2011 --file ../queries/clean_files/2011.json --jsonArray
+mongoimport --db stackoverflow --collection survey2012 --file ../queries/clean_files/2012.json --jsonArray
+mongoimport --db stackoverflow --collection survey2013 --file ../queries/clean_files/2013.json --jsonArray
+mongoimport --db stackoverflow --collection survey2014 --file ../queries/clean_files/2014.json --jsonArray
+mongoimport --db stackoverflow --collection survey2015 --file ../queries/clean_files/2015.json --jsonArray
+mongoimport --db stackoverflow --collection survey2016 --file ../queries/clean_files/2016.json --jsonArray
+mongoimport --db stackoverflow --collection survey2017 --file ../queries/clean_files/2017.json --jsonArray
