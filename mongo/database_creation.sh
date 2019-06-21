24aade71b564b628483cf88850a7352d17875886
Jose Garcia <>
2019-06-20 19:21:35 -0500

    fixing issues with data clean routine

--- mongo/database_creation.sh
+++ mongo/database_creation.sh
@@ -7,3 +7,4 @@ mongoimport --db stackoverflow --collection survey2014 --file ../queries/clean_f
 mongoimport --db stackoverflow --collection survey2015 --file ../queries/clean_files/2015.json --jsonArray
 mongoimport --db stackoverflow --collection survey2016 --file ../queries/clean_files/2016.json --jsonArray
 mongoimport --db stackoverflow --collection survey2017 --file ../queries/clean_files/2017.json --jsonArray
+mongoimport --db stackoverflow --collection data --file ../queries/clean_files/data.json --jsonArray
