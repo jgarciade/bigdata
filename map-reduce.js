884309084263105b32792cca2707570d32452c11
Jose Garcia <>
2019-06-20 19:52:28 -0500

    adding map-reduce routine to get number of user per programming language

---
+++ map-reduce.js
@@ -0,0 +1,16 @@
+var m = function () {
+    var data = this["programming_languages"];
+    if (data != NaN && data != null) {
+        data.forEach(element => {
+            if (!element.includes(" ", ",")) {
+                emit(element, 1);
+            }
+        });
+    }
+}
+
+var r = function (key, values) {
+    return Array.sum(values)
+}
+
+db.data.mapReduce(m, r, { out: 'map_res' })
